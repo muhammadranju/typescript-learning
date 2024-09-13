9795f9199718e92c75b37c2cd346dcc4c7e5315f
Muhammad Ranju <mdranju23@gmail.com>
2024-09-13 10:44:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+// let cNames: number | string = 2150;
+// cNames = "string";
+// cNames = 102;
+// console.log(cNames);
+// function sayHi(personName: string) {
+//   console.log("Hi", personName);
+// }
+// console.log(sayHi("Ranju"));
+// function waitingTime(waitingTimes, serialNumber) {
+//   let num = 0;
+//   for (let i of waitingTimes) {
+//     num += i;
+//   }
+//   let average = Math.round(num / waitingTimes.length);
+//   let peopleWaiting = serialNumber - 1;
+//   let peopleBeforeIsrat = peopleWaiting - waitingTimes.length;
+//   let isratAvgTime = average * peopleBeforeIsrat;
+//   return isratAvgTime;
+// }
+// const hello = "Hello";
+// console.log(hello);
+// let x = 10;
+// let z = x;
+// z = 20;
+// console.log(x);
+// console.log(z);
+// let a = [1, 2, 32, 4, 4, 7];
+// let b = a;
+// console.log(a);
+// console.log(b.pop());
+// console.log(b);
+// enum User {
+//   ADMIN = "admin",
+//   USER = "USER",
+// }
+// console.log(User.USER);
+// let a: number = 1;
+// a = "Gwe";
+// console.log(a);
+// let a: string;
+// // a = 12;
+// a = "jf";
+// a.toUpperCase();
+// console.log(a);
+// interface User {
+//   name: string;
+//   email: string;
+//   password: string;
+//   phone: number;
+// }
+// interface Admin extends User {
+//   admin: boolean;
+// }
+// function abc(obj: Admin) {
+//   obj.email;
+//   obj.phone.toFixed();
+//   obj.admin;
+// }
+// abc({
+//   name: "Ranju",
+//   email: "fhdf@sdfhs",
+//   password: "dhf",
+//   phone: 4041,
+// });
+let userNum = 41023;
+let userName = "Md Ranju";
+let userType = true;
+console.log(userNum);
