c92b7c12c158a5e94e9f1870d40d72426a94bfab
Muhammad Ranju <mdranju23@gmail.com>
2024-09-14 09:32:06 +0600

    feat: user defined type in userType file

---
+++ user-defined-type/userType.js
@@ -0,0 +1,10 @@
+"use strict";
+// let userId: string | number = 111;
+// console.log(userId);
+// userId = "Ranju";
+// console.log(userId);
+function userDisplay(userId) {
+    return userId;
+}
+console.log(userDisplay("Ranju"));
+console.log(userDisplay(100));
---
+++ user-defined-type/userType.ts
@@ -0,0 +1,11 @@
+// let userId: string | number = 111;
+
+// console.log(userId);
+// userId = "Ranju";
+// console.log(userId);
+
+function userDisplay(userId: string | number) {
+  return userId;
+}
+console.log(userDisplay("Ranju"));
+console.log(userDisplay(100));
