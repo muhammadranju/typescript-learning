55ef65dd992bc25e6f919c020b5753f8e5fb7ce1
Muhammad Ranju <mdranju23@gmail.com>
2024-09-14 19:41:38 +0600

    feat: added array type in TypeScript

--- arrayType/array.js
+++ arrayType/array.js
@@ -1,3 +1,4 @@
 "use strict";
 let userName = ["Ranju", "Hossain", "Muhammad", "Amin"];
+console.log(userName.sort());
 console.log(userName[0]);
--- arrayType/array.ts
+++ arrayType/array.ts
@@ -1,3 +1,4 @@
 let userName: Array<string> = ["Ranju", "Hossain", "Muhammad", "Amin"];
+console.log(userName.sort());
 
 console.log(userName[0]);
